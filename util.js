87cd56fbcab28091efd740604cb48a7546e23026
mcb419 <mcb419@gmail.com>
2018-11-20 21:13:16 -0600

    added randn, alphabetized functions

--- util.js
+++ util.js
@@ -1,3 +1,11 @@
+function randi() {
+  // randi() returns 0 or 1
+  // randi(max) returns random interger in [0, max)
+  // randi(min, max) return random integers in [min, max]
+  if(arguments.length === 0) return randi(2); // 0 or 1
+  else return Math.floor(randf(...arguments));
+}
+
 function randf() {
   // randf() returns a random number in [0, 1)
   // randf(max) returns a random number in [0, max)
@@ -10,10 +18,20 @@ function randf() {
   return min + Math.random()*(max - min);
 }
 
-function randi() {
-  // randi() returns 0 or 1
-  // randi(max) returns random interger in [0, max)
-  // randi(min, max) return random integers in [min, max]
-  if(arguments.length === 0) return randi(2); // 0 or 1
-  else return Math.floor(randf(...arguments));
+function randn(std) {
+  // randn() returns random gaussian with mean 0, std 1
+  // randn(std) returns zero-mean random gaussian with std
+  if(arguments.length === 0) std = 1.0;
+  if(this.cached) { 
+    this.cached = false;
+    return this.cachedValue; 
+  }
+  let u = 2*Math.random()-1;
+  let v = 2*Math.random()-1;
+  let r = u*u + v*v;
+  if(r === 0 || r > 1) return randn(...arguments);
+  let c = Math.sqrt(-2*Math.log(r)/r);
+  this.cachedValue = v*c*std; // cache this
+  this.cached = true;
+  return u*c*std;
 }
